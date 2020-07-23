96f5e5f6385b83bf4f6abc065c61198bcdc3fdaa
Pratyush2710 <pratyushhg@gmail.com>
2020-07-23 17:37:47 +0530

    Error, login page, background animation

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/context";
 const Dashboard = () => {
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <Navbar />
+      <Search />
+      <Info />
+      <User />
+      <Repos />
     </main>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import styled from 'styled-components';
-import loginImg from '../images/login-img.svg';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
+import loginImg from "../images/login-img.svg";
 const Login = () => {
-  return <h2>login page</h2>;
+  return (
+    <Wrapper>
+      <div className="container">
+        <img src={loginImg} alt="git user" />
+        <h1>Github User</h1>
+        <button className="btn">Login</button>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
