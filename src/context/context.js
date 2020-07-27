1d32bec21f6b9c8adadd4efc7466f6b7756f116d
Pratyush2710 <pratyushhg@gmail.com>
2020-07-28 04:03:19 +0530

    promise.allsettled for async await

--- src/context/context.js
+++ src/context/context.js
@@ -20,15 +20,32 @@ const GithubProvider = ({ children }) => {
   const [error, setError] = useState({ show: false, msg: "" });
 
   const searchGithubUser = async (user) => {
-    console.log(user);
     toggleError();
     setIsLoading(true);
     const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
       console.log(error)
     );
-    console.log(response);
     if (response) {
       setGithubUser(response.data);
+      console.log("*********", githubUser);
+      const { login, followers_url } = response.data;
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = "fulfilled";
+          if (repos.status === status) {
+            setGithubRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setGithubFollowers(followers.value.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } else {
       toggleError(true, "No user found!");
     }
