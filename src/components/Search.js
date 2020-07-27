cd397f0c3853ac0ec8d394f516e04a993a1d81ac
Pratyush2710 <pratyushhg@gmail.com>
2020-07-27 18:36:13 +0530

    search component added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState("");
+  //get things from global context
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+      // to reset the name after submission
+      // setUse('');
+    }
+    console.log(user);
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="Enter github user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <button type="submit"> Search</button>
+          </div>
+        </form>
+        <h3>requests: 60/60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
