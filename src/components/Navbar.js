e35f37d59316548ebe9a975e88765692920c2765
Pratyush2710 <pratyushhg@gmail.com>
2020-07-29 22:53:56 +0530

    Login logout added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,17 @@ import styled from "styled-components";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
-  return <Wrapper>navbar component</Wrapper>;
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    isLoading,
+  } = useAuth0();
+
+  const isUser = isAuthenticated && user;
+  console.log(isAuthenticated, user);
+  return <Wrapper></Wrapper>;
 };
 
 const Wrapper = styled.nav`
--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-74a0k4yb.us.auth0.com"
+      clientId="ZMttvEWMLZ1ZEnmGaA7D5gihS3zkfAEx"
+      redirectUri={window.location.origin}>
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
