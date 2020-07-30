e1c5da9c40d2d0d49cdce50924e1c1567ad8ae82
Pratyush2710 <pratyushhg@gmail.com>
2020-07-30 23:37:23 +0530

    loader gif changed

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import loadingGif from "../images/preloader.gif";
+import loadingGif from "../images/loading-dots1.gif";
 import styled from "styled-components";
 function AuthWrapper({ children }) {
   const { isLoading, error } = useAuth0();
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Info, Repos, User, Search, Navbar } from "../components";
-import loadingImage from "../images/preloader.gif";
+import loadingImage from "../images/loading-dots1.gif";
 import { GithubContext } from "../context/context";
 const Dashboard = () => {
   const { isLoading } = React.useContext(GithubContext);
